af130074817fadae98f3d9cee89adf1f09ead34d
ROHITH SRINIVAAS M <ROHITH SRINIVAAS M>
2017-04-09 14:24:15 +0530

    Updated grabcut algo

--- grabcut.py
+++ grabcut.py
@@ -7,6 +7,7 @@ from tensorflow.python.framework import dtypes
 import numpy as np
 import cv2
 import glob
+totaltime=0
 
 text_file =  open("weights1.txt", "w") 
 #file containing the path to images and labels
@@ -74,10 +75,6 @@ for i in range(20000):
 	train_step.run(feed_dict={X:batch[0], Y:batch[1], keep_prob:0.5})
 print ("Test Accuracy :%.9f"%accuracy.eval(feed_dict={X:MNIST.test.images, Y:MNIST.test.labels, keep_prob:1.0}))
 '''
-no_epoch_run = 0
-
-print("\n\n"+str(no_epoch_run) + "\n\n")
-#reading file and extracting path and labels
 with open(filename, 'r') as File:
 	infoFile = File.readlines()	#reading lines from files
 	for line in infoFile: #reading line by line
@@ -94,7 +91,7 @@ NumFiles = len(filenames)
 #Converting filnames into tensor
 tfilenames = ops.convert_to_tensor(filenames, dtype = dtypes.string)
 tlabels = ops.convert_to_tensor(labels, dtype=dtypes.string)
-
+#print (tlabels)
 #creating a queue which contains the list of files to read and the values of labels
 filename_queue = tf.train.slice_input_producer([tfilenames, tlabels], num_epochs=10, shuffle=False, capacity = NumFiles)
 #reading image files and decoding them
@@ -103,32 +100,39 @@ decodedIm = tf.image.decode_jpeg(rawIm)
 lbl = []
 #extracting the labels queue
 label_queue = filename_queue[1]
-
+#print (label_queue)
 #Initializing global and local variable initializers
-lbl_array = []
 init_op = tf.group(tf.local_variables_initializer(), tf.global_variables_initializer())
 #Creating an interactive session to run in python file
 label_value = []
-
-while(no_epoch_run <= 1):
-	sess = tf.InteractiveSession()
-	sess.run(init_op)
-	no_epoch=0
-	train_step = tf.train.GradientDescentOptimizer(0.02).minimize(loss)
-	with sess.as_default():
-		
-		#start populating the filename queue
-		coord = tf.train.Coordinator()
-		threads = tf.train.start_queue_runners(coord = coord)
-		writer = tf.summary.FileWriter('./graphs', sess.graph)
+sess = tf.InteractiveSession()
+sess.run(init_op)
+Tolerance = 0
+no_epoch=0
+loss_to_be_minimized = 0
+label_counter = 0
+train_step = tf.train.GradientDescentOptimizer(1e-8).minimize(loss)
+Train_Checker = []
+with sess.as_default():
+	
+	#start populating the filename queue
+	coord = tf.train.Coordinator()
+	threads = tf.train.start_queue_runners(coord = coord)
+	writer = tf.summary.FileWriter('./graphs', sess.graph)
+	flag = 0 #epoch
+	lbl_array = []
+	img_array = []
+	
+	while(True):
+		flag = flag + 1
 		begtime = time.time()
-		flag = 0
 		i = 0
+		Train_Checker.append(loss_to_be_minimized)	#Previous loss function
+		loss_to_be_minimized = 0
 		for i in range(NumFiles):
-			nm, image, lb = sess.run([filename_queue[0], decodedIm, label_queue])	
-			#plt.imshow(image)
-			#plt.show()
-			if no_epoch_run<1:
+			
+			if flag<=1:
+				nm, image, lb = sess.run([filename_queue[0], decodedIm, label_queue])					
 				if i == 0:
 					l1 = labels[(4*i)+0]
 					l2 = labels[(4*i)+1]
@@ -138,32 +142,72 @@ while(no_epoch_run <= 1):
 					lbl_array.append(lbl)					
 				else:
 					lbl = labels[i]
-					lbl_array.append(lbl)			
-			input_image = (sess.run(tf.reshape(image, [-1,196608])))			
-			ip_img = input_image
-			labels =np.reshape(labels, (-1,4))			
-			endtime = time.time()
-			print (lbl)			
-			lbl = np.reshape(lbl, (-1,4))
-			print(sess.run(fc1, feed_dict={x:ip_img, y_:[lbl_array[i]]}))			
-			while(sess.run(loss, feed_dict={x:ip_img, y_:[lbl_array[i]]}) > 1.0):
-				no_epoch = no_epoch + 1
-				train_step.run(feed_dict={x:input_image, y_:[lbl_array[i]]})
-				
-				if (no_epoch%10)==0:
-					if flag == 0:
-						print ("\n"+str(no_epoch/10)+"\n")
-						flag = flag + 1
-					print (str(no_epoch_run)+" "+ str(no_epoch)+" "+str(i) + " Last Feed Neural(Prediction): \n"+ str(sess.run((fc1), feed_dict={x:ip_img, y_:[lbl_array[i]]}))+"\n Label: "+ str(lbl) +"\n Loss Function: "+ str(sess.run(loss, feed_dict={x:ip_img, y_:[lbl_array[i]]})) +str("\n Time Taken: ") + str(endtime-begtime) + str(" s"))
-					
-					
-						
-	no_epoch_run = no_epoch_run + 1
-	text_file.write(str(sess.run(w1)) +"\n" + str(sess.run(w2))+ "\n" + str(sess.run(w_fc1)))
+					lbl_array.append(lbl)
+				input_image = (sess.run(tf.reshape(image, [196608])))
+				img_array.append(input_image)
+				ip_img = input_image
+				labels =np.reshape(labels, (-1,4))
+			
+			#plt.imshow(image)
+			#plt.title(sess.run(labels))
+			#plt.show()
+			#lbl = np.reshape(lbl, (-1,4))
+		    
+			no_of_times_run = 0
+			#no_epoch = no_epoch + 1
+			while(True):
+				train_step.run(feed_dict={x:[img_array[i]], y_:[lbl_array[i]]})
+				no_of_times_run = no_of_times_run + 1
+				if no_of_times_run>3:
+					break
+			loss_to_be_minimized = loss_to_be_minimized +  sess.run(loss, feed_dict={x:[img_array[i]], y_:[lbl_array[i]]})
+			#print(input_image.shape+ " " + labels.shape
+			#if (no_epoch%10)==0:
+			#	if flag == 0:
+			#		print ("\n"+str(no_epoch/10)+"\n")
+			#		flag = flag + 1
+			#print (str(i) + " Last Feed Neural(Prediction): \n"+ str(sess.run((fc1), feed_dict={x:ip_img, y_:lbl}))+"\n Label: "+ str(lbl) +"\n Loss Function: "+ str(sess.run(loss, feed_dict={x:ip_img, y_:lbl})) +str("\n Time Taken: ") + str(endtime-begtime) + str(" s"))
+		endtime = time.time()
+		totaltime = totaltime + (endtime-begtime)
+
+		print ("Epoch: "+ str(flag)+ "\t"+ "Total Error: "+str(loss_to_be_minimized)+ "\t"+ "Tolerance: "+ str(Tolerance)+ "\t" + "Time Taken: "+ str(endtime- begtime))
+					#text_file.write(str(loss)+" "+str(no_epoch)+" "+ "\n")
+	
+		text_file.write(str(flag) + " "+ str(loss_to_be_minimized))
+		#plt.axis([0, 100, 0, 1e5])
+
+		#plt.ion()
+		#y = loss_to_be_minimized
+		#plt.xlabel("Epochs")
+		#plt.ylabel("Total_loss(L2 Loss)")
+		#plt.title("Loss Vs Epochs")
+		#plt.scatter(flag, y)
+		#plt.pause(0.05)
+		Train_Checker.append(loss_to_be_minimized) #Updated Loss function
+		if (loss_to_be_minimized < 1000):
+			break
+		if (Train_Checker[0] <= Train_Checker[1] and flag > 1):
+			Tolerance = Tolerance + 1
+			if (Tolerance > 30):	
+				break
+		del Train_Checker[:]
 	coord.request_stop()
 	coord.join(threads)
 	writer.close()
 	
+'''		image_conv = sess.run(maxp1, feed_dict={x:image})
+		op_image = tf.to_float(tf.reshape(image_conv, [128,128,3]))
+		plt.imshow(sess.run(op_image))
+		plt.title("Conv-Layer-1")
+		plt.show()
+		image_conv2 = sess.run(maxp2, feed_dict={x:ip_img})
+		op_image = tf.to_float(tf.reshape(image_conv2,[64,64,3]))
+		op_image = tf.image.resize_images(op_image, [128,128])
+		plt.imshow(sess.run(op_image))
+		plt.title("Conv-Layer-2")
+		plt.show()
+'''
+print("TotalTime Taken: " + str(totaltime))
 
 text_file.close()
 for imga in glob.glob("./test/*.jpg"):
@@ -185,7 +229,7 @@ for imga in glob.glob("./test/*.jpg"):
 	[[a,b,c,d]] = prediction
 	rect = (int(a),int(b),int(c),int(d))
 	print (rect)
-	cv2.grabCut(img,mask,rect,bgdModel,fgdModel,10,cv2.GC_INIT_WITH_RECT)
+	cv2.grabCut(img,mask,rect,bgdModel,fgdModel,50,cv2.GC_INIT_WITH_RECT)
 	mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')#cannot understand
 	img = img*mask2[:,:,np.newaxis]
 	cv2.imshow("Image",img)
